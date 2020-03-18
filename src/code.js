5a67b30d0eb808e143c2517a6ecff5814c2aa03d
Mihir Parikh <mparikh@l002495.lan>
2020-03-18 14:31:19 -0700

    Make the words in the list selectable and also clean up the code some more

--- src/code.js
+++ src/code.js
@@ -18,25 +18,72 @@ Dictionary.forEach((word) => {
 
 // process user input
 var inputEl = document.getElementById("textinput");
-const zeroState = '<span>Begin Typing..</span>';
-document.getElementById("textoutput").innerHTML = zeroState;
+const outputBlock = document.getElementById('textoutput');
+const zeroState = '<div id="zerospan"><span>Begin Typing...</span><br></div>';
+
+function removeElementById(id) {
+    let elem = document.getElementById(id);
+    if (elem) {
+        elem.parentNode.removeChild(elem);
+    }
+}
+
+// future proof
+var wordSelectHandler = (payload) => {
+    console.log("selected : " + payload);
+    inputEl.focus();
+    inputEl.value = payload;
+    inputHandler(25);
+};
+
+function inputHandler(pause) {
+    pause = pause || 25; // 25ms by default
+
+    setTimeout(() => {    
+        if (document.activeElement === inputEl) { 
+            if (inputEl.value.length) {
+                // lowercase all input
+                let inputVal = inputEl.value;
+                inputVal = inputVal + ''; // coerce to string
+                inputVal = inputVal.toLowerCase();
+
+                var predictions = root.predictWord(inputVal);
+
+                removeElementById('zerospan'); // remove zerostate node
+                removeElementById('innerdiv'); // remove prior innerdiv
+                
+                // new innerdiv
+                const innerDiv = document.createElement('DIV');
+                innerDiv.id = "innerdiv";
+            
+                let wordCount = '<span id="wordcount">Word Count: ' + predictions.length + '</span><br>';
+                innerDiv.insertAdjacentHTML('afterbegin', wordCount);
+                outputBlock.appendChild(innerDiv);
+
+                if (predictions.length > 0) {   
+                    predictions.forEach((word) => {
+                        let span = document.createElement('SPAN');
+                        span.innerHTML = word;
+                        span.id = word;
+                        span.style.cursor = "pointer";
+                        span.onclick = function() {
+                            wordSelectHandler(word);
+                        }
+                        innerDiv.insertAdjacentElement('beforeend', span);
+                        innerDiv.insertAdjacentHTML('beforeend', '<br>');
+                    });     
+                }
+            } else {
+                // when input field is blank, go back to zero message
+                removeElementById('innerdiv');
+                outputBlock.innerHTML = zeroState;
+            }
+        }
+    }, pause);
+}
 
 // keypress event
 document.addEventListener("keydown", (e) => {
-    setTimeout(() => {    
-        if (document.activeElement === inputEl) {
-            var predictions = root.predictWord(inputEl.value);
-            let displayPredictions = zeroState;
-            if (inputEl.value.length) {
-                displayPredictions = "Word Count: " + predictions.length + "<BR>"; 
-                if (predictions.length > 0) {
-                    displayPredictions += predictions.join('<BR>');          
-                }
-            } else {
-                displayPredictions = zeroState;
-            }
-            document.getElementById("textoutput").innerHTML = displayPredictions;
-        }
-    }, 10);
+    console.log("keydown event: " + e.key);
+    inputHandler(25);
 });
-
